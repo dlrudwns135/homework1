56b1ea9c74fb747ac333cabbcb89de136d340b11
dlrudwns135 <dlrudwns135@hanmail.net>
2016-04-22 14:04:00 +0900

    init

---
+++ src/homework1/Pair.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class Pair <T1, T2>{
+	T1 t1;
+	T2 t2;
+	public Pair(T1 t1, T2 t2)
+	{
+		this.t1 = t1;
+		this.t2 = t2;
+	}
+}
---
+++ src/homework1/Tetscse.java
@@ -0,0 +1,10 @@
+package homework1;
+
+public class Tetscse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
