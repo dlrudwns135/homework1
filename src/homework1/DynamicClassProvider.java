a67d4fceb47efbad5b6027df253dd28ed04405e7
renil <renil@renil-PC>
2016-05-06 00:54:57 +0900

    dd

--- src/homework1/DynamicClassProvider.java
+++ src/homework1/DynamicClassProvider.java
@@ -1,5 +1,46 @@
 package homework1;
 
-public class DynamicClassProvider {
 
+import java.lang.reflect.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DynamicClassProvider {
+	Map<Pair<String, String>, String> m_cls = null;
+	Map<Pair<String, String>, Class<?>> m_cache = null;
+	
+	public static void register(String nick, String creater, String path)
+	{
+		Pair<String, String> temp = new Pair<String, String>(nick, creater);
+		Class cl = null;
+		try
+		{
+			cl = Class.forName("DynamicClassProvider");
+			Field fl = cl.getDeclaredField("m_cls");
+			Object ob;
+			
+			ob = fl.get(new DynamicClassProvider());
+			Class cls[] = new Class[2];
+			cls[0] = Object.class;
+			cls[1] = Object.class;
+			
+			Method m = HashMap.class.getMethod("put", cls);
+			
+			Object o[] = new Object[2];
+			o[0] = temp;
+			o[1] = path;
+			m.invoke(ob, o);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.toString());
+		}
+		
+	
+	}
+	
+	public static Object newInstance(String nick, String creater)
+	{
+		return creater;
+	}
 }
--- src/homework1/Tetscse.java
+++ src/homework1/Tetscse.java
@@ -4,7 +4,9 @@ public class Tetscse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		DynamicClassProvider.register("Trident", "M", "homework1.Pair");
+		
+		Object O = DynamicClassProvider.newInstance("Trident", "M");
+		((IPrintable)O).printName();
 	}
-
 }
